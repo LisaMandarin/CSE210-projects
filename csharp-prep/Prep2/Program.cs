927627672fed99978521df019e92686bbdb4c67a
LisaMandarin <lisa_chuang@mail2000.com.tw>
2023-04-20 01:31:00 +0800

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,66 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Ask the user for their grade percentage
+        Console.Write("What is your grade percentage? ");
+
+        //Convert the grade percentage to integer
+        string gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+
+        //Turn into the grade letter according the user's input
+        string letter;
+        string sign;
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >=80)
+        {
+            letter = "B";
+        }
+        else if (number >=70)
+        {
+            letter = "C";
+        }
+        else if (number >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        //Add + or - next to the grade
+        if (number >=93 || number <60)
+        {
+            sign = "";
+        }
+        else if (number % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (number % 10 <3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"You've got a(n) {letter}{sign}.");
+
+        //Tell the user if he has passed
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations.  You have passed.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, You didn't pass.  Try harder and you'll pass.");
+        }
     }
 }
