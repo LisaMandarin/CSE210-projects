18b7fe1f7f1e15d744c70d82d6c359b938edbc81
LisaMandarin <lisa_chuang@mail2000.com.tw>
2023-04-20 00:36:47 +0800

    Added camel case variable

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,9 +6,9 @@ class Program
     {
         //Ask the user for their name.
         Console.Write("What is your first name?");
-        string f_name = Console.ReadLine();
+        string fName = Console.ReadLine();
         Console.Write("What is your last name?");
-        string l_name = Console.ReadLine();
-        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
+        string lName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
